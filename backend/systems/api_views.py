236ce2bdcf14e8bc6bcaa42536abc7ad652cda7b
timor <timor@123.com>
2019-12-04 19:12:24 +0800

    fix

--- backend/systems/api_views.py
+++ backend/systems/api_views.py
@@ -5,17 +5,26 @@ from systems.models import *
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework import exceptions
-from django.db.models import Q
-from systems.menus import get_menus_by_user
+from systems.menus import get_menus_by_user, set_menu
+
 
 class getuserinfo(APIView):
     """登录"""
 
     def get(self, request, *args, **kwargs):
-        ret = {'code': 1000, 'msg': "成功", 'data':[]}
+        ret = {'code': 1000, 'msg': "成功", 'data': []}
         try:
             user = request.user
-            ret['data'] = get_menus_by_user(user)
+            user_obj = User.objects.get(username=user)
+            menu_data = get_menus_by_user(user)
+
+            if len(menu_data) > 0:
+                topmenuid = menu_data[0].parent_id
+                if topmenuid == 0:
+                    topmenuid = menu_data[0].id
+            menus = set_menu(menu_data, topmenuid)
+            print(menus)
+            ret['data'] = {'menus': menus, 'realname': user_obj.realname, 'avatar': user_obj.avatar}
         except exceptions as e:
             ret['code'] = 1002
             ret['msg'] = "请求异常"
--- backend/systems/menus.py
+++ backend/systems/menus.py
@@ -41,40 +41,63 @@ def getSuperAdminMenu():
 
 # 获取管理员权限下所有菜单
 def get_menus_by_user(user):
-    allmenus = Menu.objects.all()
     user_role_list = User.objects.filter(username=user).values('roles')
     user_menu_list = Role.objects.filter(id__in=user_role_list).values('menus')
-    user_all_menu = dict()
-    nocache = False
 
-    menuMapAll = {}
-    menuMap = {}
-    for item in allmenus:
-        menuMapAll[item.id] = item
-
+    menuMap = dict()
     for item in user_menu_list:
         menu_info = Menu.objects.get(id=item['menus'])
-        # menu = {
-        #     'path': menu_info.curl,
-        #     'component': menu_info.code,
-        #     'name': menu_info.code,
-        #     'meta': {'title': menu_info.name, 'icon': menu_info.icon, 'nocache': nocache},
-        #     'children': []
-        # }
         menuMap[menu_info.id] = menu_info
 
     for item in user_menu_list:
         menu_info = Menu.objects.get(id=item['menus'])
         if menu_info.parent in menuMap:
             continue
-        aaa = setMenuUp(menuMapAll, menu_info.parent_id, menuMap)
-        print(aaa)
-    return {1:2}
+        user_menus = find_menu_daddy(menu_info.parent_id, menuMap)
+    return list(user_menus.values())
 
-def setMenuUp(menuMapAll, menuid, menuMap):
-    if menuid in menuMapAll:
-        mid = menuMapAll[menuid].id
+
+def find_menu_daddy(menuid, menuMap):
+    obj = Menu.objects.filter(id=menuid).first()
+    if obj:
+        mid = obj.id
         if mid not in menuMap:
-            menuMap[mid] = menuMapAll[menuid]
-            setMenuUp(menuMapAll, menuMapAll[menuid].parent_id, menuMap)
+            menuMap[mid] = obj
+            find_menu_daddy(obj.parent_id, menuMap)
             return menuMap
+
+
+def set_menu(menus, parent_id):
+    nocache = False
+    all_menus = []
+    len(menus)
+    for item in menus:
+        menu = {
+            'path': item.curl,
+            'component': item.code,
+            'name': item.code,
+            'meta': {'title': item.name, 'icon': item.icon, 'nocache': nocache},
+            'children': []
+        }
+        if item.type == 3:
+            menu['hidden'] = True
+
+        # 查询是否有子级
+        menu_children = set_menu(menus, item.id)
+        if len(menu_children) > 0:
+            menu['children'] = menu_children
+
+        if item.type == 2:
+            # 添加子级首页，有这一级NoCache才有效
+            menu_index = {
+                'path': 'index',
+                'component': item.code,
+                'name': item.code,
+                'meta': {'title': item.name, 'icon': item.icon, 'nocache': nocache},
+                'children': []
+            }
+            menu['children'].append(menu_index)
+            menu['meta'] = []
+
+        all_menus.append(menu)
+    return all_menus
